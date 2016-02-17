58d52604989397de62bcbe47a80501597ba2673a
erinsullens <sullense@oregonstate.edu>
2016-02-16 17:54:21 -0800

    testing button issues

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -14,12 +14,7 @@ public abstract class Game {
     public java.util.List<java.util.List<Card>> cols = new ArrayList<>();
 
 
-    public Game(){
-        cols.add(new ArrayList<Card>());
-        cols.add(new ArrayList<Card>());
-        cols.add(new ArrayList<Card>());
-        cols.add(new ArrayList<Card>());
-    }
+
 
 
     public abstract void buildDeck();
@@ -31,6 +26,7 @@ public abstract class Game {
     }
 
     public void dealFour() {
+        System.out.println("tried to deal");
         for(int i = 0; i < 4; i++){
             cols.get(i).add(deck.get(deck.size()-1));
             deck.remove(deck.size()-1);
@@ -50,6 +46,7 @@ public abstract class Game {
     }
 
     public void remove(int columnNumber) {
+        System.out.println("tried to remove");
         if(colHasCards(columnNumber)) {
             Card c = getTopCard(columnNumber);
             boolean removeCard = false;
